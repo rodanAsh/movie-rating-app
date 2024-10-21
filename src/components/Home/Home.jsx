687a8b651c4d5ea25665f2135d6d1d75ebf1116d
rodanAsh <aakashash2002@gmail.com>
2024-10-21 13:59:02 +0530

    feat ~ function to store api result in redux store

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import MovieListing from '../MovieListing/MovieListing.jsx'
+import movieApi from '../../common/api/movieApi.js'
+import {apiKey} from '../../common/api/MovieApikey.js'
+import { useDispatch } from 'react-redux'
+import { addMovies } from '../../features/movies/movieSlice.js'
 
 const Home = () => {
+  const movieText = "One piece"
+  const dispatch = useDispatch()
+  {/* function to hit omdb api */}
+  const fetchMovies = async() => {
+    const response = await movieApi.get(`?apiKey=${apiKey}&s=${movieText}&type=movie`)
+      .catch((err) => console.error(err))
+
+    dispatch(addMovies(response.data))
+  }
+
+  useEffect(() => {
+    fetchMovies()
+  },[])
+
   return (
     <section>
       <div className="banner-img">
